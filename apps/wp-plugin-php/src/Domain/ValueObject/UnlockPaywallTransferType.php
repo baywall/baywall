d8c6366d816a6c94ce8f84413589cbdda24e5962
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-07-28 07:09:44 +0000

    refactor(wp-plugin): UnlockPaywallTransferTypeIDクラスを削除し、UnlockPaywallTransferTypeクラスに統合

--- apps/wp-plugin-php/src/Domain/ValueObject/UnlockPaywallTransferType.php
+++ apps/wp-plugin-php/src/Domain/ValueObject/UnlockPaywallTransferType.php
@@ -3,9 +3,15 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Domain\ValueObject;
 
-use Cornix\Serendipity\Core\Constant\UnlockPaywallTransferTypeID;
-
 class UnlockPaywallTransferType {
+
+	// 以下の定義はコントラクトで定義されている内容と一致させるようにしてください。
+	/** 販売手数料 */
+	private const HANDLING_FEE = 1;
+	/** 販売者の売上 */
+	private const SELLER_PROFIT = 2;
+	/** アフィリエイト報酬 */
+	private const AFFILIATE_REWARD = 3;
 
 	private function __construct( int $unlock_paywall_transfer_type_id ) {
 		self::checkValidTransferTypeID( $unlock_paywall_transfer_type_id );
@@ -23,16 +29,8 @@ class UnlockPaywallTransferType {
 	}
 
 	private static function checkValidTransferTypeID( int $unlock_paywall_transfer_type_id ): void {
-		if ( ! in_array( $unlock_paywall_transfer_type_id, self::allTransferTypeIDs(), true ) ) {
+		if ( $unlock_paywall_transfer_type_id < self::HANDLING_FEE || $unlock_paywall_transfer_type_id > self::AFFILIATE_REWARD ) {
 			throw new \InvalidArgumentException( '[F468C1FA] Invalid unlock paywall transfer type ID: ' . $unlock_paywall_transfer_type_id );
 		}
-	}
-
-	private function allTransferTypeIDs(): array {
-		$reflection = new \ReflectionClass( UnlockPaywallTransferTypeID::class );
-		$constants  = $reflection->getConstants();
-		/** @var int[] */
-		$all_transfer_type_ids = array_values( $constants );
-		return $all_transfer_type_ids;
 	}
 }
