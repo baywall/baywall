2e33266286e3872d7fc1189ec0b901c7e8c2bce4
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-08-04 01:01:13 +0000

    refactor(wp-plugin): OracleTableSeedからChainIdValueの参照を削除

--- apps/wp-plugin-php/src/Infrastructure/WordPress/Database/Migrations/OracleTableSeed.php
+++ apps/wp-plugin-php/src/Infrastructure/WordPress/Database/Migrations/OracleTableSeed.php
@@ -3,13 +3,23 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Infrastructure\WordPress\Database\Migrations;
 
-use Cornix\Serendipity\Core\Infrastructure\Constant\ChainIdValue;
+use Cornix\Serendipity\Core\Domain\ValueObject\Address;
+use Cornix\Serendipity\Core\Domain\ValueObject\ChainID;
+use Cornix\Serendipity\Core\Domain\ValueObject\Symbol;
 use Cornix\Serendipity\Core\Infrastructure\System\Environment;
+use Cornix\Serendipity\Core\Infrastructure\Web3\Registry\ChainIdRegistry;
 use Cornix\Serendipity\Core\Infrastructure\WordPress\Database\Migrations\Base\MigrationBase;
 use Cornix\Serendipity\Core\Infrastructure\WordPress\Database\Migrations\Base\MigratorBase;
 use Cornix\Serendipity\Core\Repository\Name\TableName;
 use wpdb;
 
+
+// Oracleのアドレスは以下のURLで確認可能
+// https://docs.chain.link/data-feeds/price-feeds/addresses
+
+// ■ Fiatのアドレス確認方法
+// 以下のコマンドでFiatのOracleアドレスを確認可能
+// curl -s https://reference-data-directory.vercel.app/feeds-mainnet.json | jq '.[] | select(.docs.assetClass == "Fiat")'
 
 class OracleTableSeed extends MigratorBase {
 
@@ -29,7 +39,26 @@ class OracleTableSeed extends MigratorBase {
 // --------------------------------------------------------------------------------
 
 /** @internal */
-class OracleTableSeed_0_0_1 extends MigrationBase {
+abstract class OracleTableSeedBase extends MigrationBase {
+	protected function add( ChainID $chain_id, string $address_value, string $base_symbol_value, string $quote_symbol_value ): void {
+		// Address, Symbolオブジェクトを経由することでフォーマットチェックを実施
+		$address      = Address::from( $address_value );
+		$base_symbol  = Symbol::from( $base_symbol_value );
+		$quote_symbol = Symbol::from( $quote_symbol_value );
+		$this->insert(
+			$this->tableName(),
+			array(
+				'chain_id'     => $chain_id->value(),
+				'address'      => $address->value(),
+				'base_symbol'  => $base_symbol->value(),
+				'quote_symbol' => $quote_symbol->value(),
+			)
+		);
+	}
+}
+
+/** @internal */
+class OracleTableSeed_0_0_1 extends OracleTableSeedBase {
 
 	public function __construct( Environment $environment ) {
 		$this->environment = $environment;
@@ -37,57 +66,20 @@ class OracleTableSeed_0_0_1 extends MigrationBase {
 	private Environment $environment;
 
 	public function up(): void {
-
-		$Record = new class( 0, '', '', '' ) {
-			public function __construct(
-				int $chain_id,
-				string $address,
-				string $base_symbol,
-				string $quote_symbol
-			) {
-				$this->chain_id     = $chain_id;
-				$this->address      = $address;
-				$this->base_symbol  = $base_symbol;
-				$this->quote_symbol = $quote_symbol;
-			}
-			public int $chain_id;
-			public string $address;
-			public string $base_symbol;
-			public string $quote_symbol;
-		};
-
-		$records = array();
-
-		// Oracleのアドレスは以下のURLで確認可能
-		// https://docs.chain.link/data-feeds/price-feeds/addresses
-
 		// ■ Fiat
-		// 以下のコマンドでFiatのOracleアドレスを確認可能
-		// curl -s https://reference-data-directory.vercel.app/feeds-mainnet.json | jq '.[] | select(.docs.assetClass == "Fiat")'
 		if ( 'ja' === substr( get_locale(), 0, 2 ) || $this->environment->isDevelopment() ) {
 			// サイトの言語が日本語の場合、もしくは開発モード時は、`JPY / USD`を登録
-			$records[] = new $Record( ChainIdValue::ETH_MAINNET, '0xBcE206caE7f0ec07b545EddE332A47C2F75bbeb3', 'JPY', 'USD' );
+			$this->add( ChainIdRegistry::ethMainnet(), '0xBcE206caE7f0ec07b545EddE332A47C2F75bbeb3', 'JPY', 'USD' );
 		}
+
 		// ■ Crypto
-		$records[] = new $Record( ChainIdValue::ETH_MAINNET, '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419', 'ETH', 'USD' );
+		$this->add( ChainIdRegistry::ethMainnet(), '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419', 'ETH', 'USD' );
 
 		// テスト中はプライベートネットのOracleを登録
 		if ( $this->environment->isTesting() ) {
 			// プライベートネットのOracleを登録
-			$records[] = new $Record( ChainIdValue::PRIVATENET_L1, '0x3F3B6a555F3a7DeD78241C787e0cDD8E431A64A8', 'ETH', 'USD' );
-			$records[] = new $Record( ChainIdValue::PRIVATENET_L1, '0xc886d2C1BEC5819b4B8F84f35A9885519869A8EE', 'JPY', 'USD' );
-		}
-
-		foreach ( $records as $record ) {
-			$this->insert(
-				$this->tableName(),
-				array(
-					'chain_id'     => $record->chain_id,
-					'address'      => $record->address,
-					'base_symbol'  => $record->base_symbol,
-					'quote_symbol' => $record->quote_symbol,
-				)
-			);
+			$this->add( ChainIdRegistry::privatenetL1(), '0x3F3B6a555F3a7DeD78241C787e0cDD8E431A64A8', 'ETH', 'USD' );
+			$this->add( ChainIdRegistry::privatenetL1(), '0xc886d2C1BEC5819b4B8F84f35A9885519869A8EE', 'JPY', 'USD' );
 		}
 	}
 
