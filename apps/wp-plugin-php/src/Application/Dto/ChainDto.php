dd0b5f42643df12ee510d7973b3686d252710244
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-08-06 06:50:44 +0000

    refactor(wp-plugin): ChainDto リファクタ

--- apps/wp-plugin-php/src/Application/Dto/ChainDto.php
+++ apps/wp-plugin-php/src/Application/Dto/ChainDto.php
@@ -2,41 +2,17 @@
 
 namespace Cornix\Serendipity\Core\Application\Dto;
 
-use Cornix\Serendipity\Core\Domain\Entity\Chain;
-
 class ChainDto {
 
-	private function __construct( int $id, ?string $rpc_url, string $confirmations, int $network_category_id ) {
+	public function __construct( int $id, ?string $rpc_url, string $confirmations, int $network_category_id ) {
 		$this->id                  = $id;
 		$this->rpc_url             = $rpc_url;
 		$this->confirmations       = $confirmations;
 		$this->network_category_id = $network_category_id;
 	}
 
-	private int $id;
-	private ?string $rpc_url;
-	private string $confirmations;
-	private int $network_category_id;
-
-	public function id(): int {
-		return $this->id;
-	}
-	public function rpcUrl(): ?string {
-		return $this->rpc_url;
-	}
-	public function confirmations(): string {
-		return $this->confirmations;
-	}
-	public function networkCategoryId(): int {
-		return $this->network_category_id;
-	}
-
-	public static function fromEntity( Chain $chain ): self {
-		return new self(
-			$chain->id()->value(),
-			$chain->rpcUrl() ? $chain->rpcUrl()->value() : null,
-			(string) $chain->confirmations()->value(),
-			$chain->networkCategoryId()->value(),
-		);
-	}
+	public int $id;
+	public ?string $rpc_url;
+	public string $confirmations;
+	public int $network_category_id;
 }
