369fa286b78ad040d50298c7e1393c94ad024fc8
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-08-06 06:57:52 +0000

    refactor(wp-plugin): PostDto リファクタ

--- apps/wp-plugin-php/src/Application/Dto/PostDto.php
+++ apps/wp-plugin-php/src/Application/Dto/PostDto.php
@@ -2,29 +2,13 @@
 
 namespace Cornix\Serendipity\Core\Application\Dto;
 
-use Cornix\Serendipity\Core\Domain\Entity\Post;
-
 class PostDto {
 
-	private function __construct( int $id, string $title ) {
+	public function __construct( int $id, string $title ) {
 		$this->id    = $id;
 		$this->title = $title;
 	}
 
-	private int $id;
-	private string $title;
-
-	public function id(): int {
-		return $this->id;
-	}
-	public function title(): string {
-		return $this->title;
-	}
-
-	public static function fromEntity( Post $post, string $title ): self {
-		return new self(
-			$post->id()->value(),
-			$title
-		);
-	}
+	public int $id;
+	public string $title;
 }
