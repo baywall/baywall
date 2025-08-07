22d2c014e492a34f9d70ad1a83100eba3279c6d3
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-08-07 02:55:37 +0000

    refactor(wp-plugin): TokenResolver周りのリファクタ

--- apps/wp-plugin-php/src/Application/UseCase/GetTokenDtosByFilter.php
+++ apps/wp-plugin-php/src/Application/UseCase/GetTokenDtosByFilter.php
@@ -8,10 +8,11 @@ use Cornix\Serendipity\Core\Application\Dto\TokenDtoAssembler;
 use Cornix\Serendipity\Core\Domain\Entity\Token;
 use Cornix\Serendipity\Core\Domain\Repository\TokenRepository;
 use Cornix\Serendipity\Core\Domain\Specification\TokensFilter;
+use Cornix\Serendipity\Core\Domain\ValueObject\Address;
 use Cornix\Serendipity\Core\Domain\ValueObject\ChainId;
 
 /** 指定したチェーンIDに存在するトークン一覧を取得します */
-class GetTokenDtosByChainIdValue {
+class GetTokenDtosByFilter {
 
 	public function __construct( TokenRepository $token_repository ) {
 		$this->token_repository = $token_repository;
@@ -20,12 +21,19 @@ class GetTokenDtosByChainIdValue {
 	private TokenRepository $token_repository;
 
 	/** @return TokenDto[] */
-	public function handle( int $chain_id_value ): array {
-		$chain_id = ChainId::from( $chain_id_value );
-		$tokens   = ( new TokensFilter() )
-			->byChainId( $chain_id )
-			->apply( $this->token_repository->all() );
+	public function handle( ?int $filter_chain_id_value, ?string $filter_address_value ): array {
+		$filter = new TokensFilter();
 
-		return array_map( fn( Token $token ) => TokenDtoAssembler::fromEntity( $token ), $tokens );
+		if ( $filter_chain_id_value !== null ) {
+			$filter = $filter->byChainId( ChainId::from( $filter_chain_id_value ) );
+		}
+		if ( $filter_address_value !== null ) {
+			$filter = $filter->byAddress( Address::from( $filter_address_value ) );
+		}
+
+		return array_map(
+			fn( Token $token ) => TokenDtoAssembler::fromEntity( $token ),
+			$filter->apply( $this->token_repository->all() )
+		);
 	}
 }
