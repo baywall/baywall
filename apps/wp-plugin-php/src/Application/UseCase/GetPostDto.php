36886a6b9f25746f0f66f4d8a0a83eaff7090323
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-08-12 07:08:32 +0000

    refactor(wp-plugin): add GetPostDto::handleByInvoiceId

--- apps/wp-plugin-php/src/Application/UseCase/GetPostDto.php
+++ apps/wp-plugin-php/src/Application/UseCase/GetPostDto.php
@@ -4,18 +4,26 @@ declare(strict_types=1);
 namespace Cornix\Serendipity\Core\Application\UseCase;
 
 use Cornix\Serendipity\Core\Application\Dto\PostDto;
+use Cornix\Serendipity\Core\Domain\Repository\InvoiceRepository;
 use Cornix\Serendipity\Core\Domain\Repository\PostRepository;
 use Cornix\Serendipity\Core\Domain\Service\PostTitleProvider;
+use Cornix\Serendipity\Core\Domain\ValueObject\InvoiceId;
 use Cornix\Serendipity\Core\Domain\ValueObject\PostId;
 
 class GetPostDto {
-	public function __construct( PostRepository $post_repository, PostTitleProvider $post_title_provider ) {
-		$this->post_repository     = $post_repository;
-		$this->post_title_provider = $post_title_provider;
-	}
-
 	private PostRepository $post_repository;
 	private PostTitleProvider $post_title_provider;
+	private InvoiceRepository $invoice_repository;
+
+	public function __construct(
+		PostRepository $post_repository,
+		PostTitleProvider $post_title_provider,
+		InvoiceRepository $invoice_repository
+	) {
+		$this->post_repository     = $post_repository;
+		$this->post_title_provider = $post_title_provider;
+		$this->invoice_repository  = $invoice_repository;
+	}
 
 	public function handle( int $post_id ): ?PostDto {
 		$post = $this->post_repository->get( PostId::from( $post_id ) );
@@ -23,5 +31,11 @@ class GetPostDto {
 			return null;
 		}
 		return new PostDto( $post->id()->value(), $this->post_title_provider->getPostTitle( PostId::from( $post_id ) ) );
+	}
+
+	public function handleByInvoiceId( string $invoice_id_value ): ?PostDto {
+		$invoice_id = InvoiceId::from( $invoice_id_value );
+		$invoice    = $this->invoice_repository->get( $invoice_id );
+		return $this->handle( $invoice->postId()->value() );
 	}
 }
