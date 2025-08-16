4ad058b80650b777699e557114f042ffdf23fd47
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-08-16 08:42:30 +0000

    refactor(wp-plugin): rpcURL => rpcUrl

--- apps/wp-plugin-php/src/Presentation/GraphQL/Resolver/SetRpcUrlResolver.php
+++ apps/wp-plugin-php/src/Presentation/GraphQL/Resolver/SetRpcUrlResolver.php
@@ -34,7 +34,7 @@ class SetRpcUrlResolver extends ResolverBase {
 		/** @var int */
 		$chain_id_value = $args['chainId'];
 		/** @var string|null */
-		$rpc_url_value = $args['rpcURL'];
+		$rpc_url_value = $args['rpcUrl'];
 
 		// 事前にRPC URLが登録しようとしているチェーンIDと一致するかどうかを確認
 		if ( ! is_null( $rpc_url_value ) ) {
