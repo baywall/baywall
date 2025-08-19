69cfd588cde714db1c5894dc9528605643bbf0cd
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-08-19 06:35:44 +0000

    refactor(wp-plugin): invoice情報を表すDTO（販売履歴取得用）追加

---
+++ apps/wp-plugin-php/src/Application/Dto/SalesHistoryInvoiceDto.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Cornix\Serendipity\Core\Application\Dto;
+
+/** 販売履歴取得用の invoice 情報 */
+class SalesHistoryInvoiceDto {
+	public string $id;
+	public int $created_at_unix;
+	public int $post_id;
+	public PriceDto $selling_price;
+	public int $chain_id;
+
+	public function __construct( string $invoice_id, int $created_at_unix, int $post_id, PriceDto $selling_price, int $chain_id ) {
+		$this->id              = $invoice_id;
+		$this->created_at_unix = $created_at_unix;
+		$this->post_id         = $post_id;
+		$this->selling_price   = $selling_price;
+		$this->chain_id        = $chain_id;
+	}
+}
