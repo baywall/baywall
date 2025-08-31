96d44172be9935dcd503ef8c72372212cde8f434
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-08-31 12:24:37 +0000

    refactor(wp-plugin): HandleNameProvider インスタンスをDIで注入するように修正

--- apps/wp-plugin-php/src/Presentation/AdminPageHook.php
+++ apps/wp-plugin-php/src/Presentation/AdminPageHook.php
@@ -10,6 +10,12 @@ use Cornix\Serendipity\Core\Repository\Name\Slug;
 
 class AdminPageHook {
 
+	private HandleNameProvider $handle_name_provider;
+
+	public function __construct( HandleNameProvider $handle_name_provider ) {
+		$this->handle_name_provider = $handle_name_provider;
+	}
+
 	public function register(): void {
 		// 管理画面のメニュー追加。
 		add_action( 'admin_menu', array( $this, 'addActionAdminMenu' ) );
@@ -48,7 +54,7 @@ class AdminPageHook {
 		assert( is_admin() );
 
 		// 管理画面用のスクリプトを登録する際のハンドル名を取得
-		$handle_name = ( new HandleNameProvider() )->adminScript();
+		$handle_name = $this->handle_name_provider->adminScript();
 
 		// アセットファイルを読み込む
 		$asset_file_path = ( new ProjectFile( 'public/admin/index.asset.php' ) )->toLocalPath();
