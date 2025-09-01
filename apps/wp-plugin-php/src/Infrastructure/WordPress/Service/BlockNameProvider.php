389c9c812570fded3b956533161d9613ad5d508a
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-09-01 00:17:12 +0000

    refactor(wp-plugin): Configクラスでblock.jsonへのパスを定義

--- apps/wp-plugin-php/src/Infrastructure/WordPress/Service/BlockNameProvider.php
+++ apps/wp-plugin-php/src/Infrastructure/WordPress/Service/BlockNameProvider.php
@@ -9,7 +9,8 @@ class BlockNameProvider {
 
 	/** ブロックエディタで使用されるブロック名を取得します。 */
 	public function get(): string {
-		$block_json = file_get_contents( Config::ROOT_DIR . '/build/block/block.json' );
+		assert( file_exists( Config::BLOCK_JSON_PATH ), '[5F98FB07] block.json file not found.' );
+		$block_json = file_get_contents( Config::BLOCK_JSON_PATH );
 		$block      = json_decode( $block_json, true );
 		$block_name = $block['name'];
 		return $block_name;
