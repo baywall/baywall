7b683e39a4bace8cc88139cdf943d1dfea8a931e
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-09-02 03:08:42 +0000

    refactor(wp-plugin): ClassNameクラスをClassNameProviderに変更し、定数をConfigで定義するように変更

---
+++ apps/wp-plugin-block/src/infrastructure/dom/service/ClassNameProvider.ts
@@ -0,0 +1,8 @@
+import { Config } from '../../../constant/Config';
+
+export class ClassNameProvider {
+	/** ブロックの要素に指定するCSSのクラス名 */
+	public get block(): string {
+		return Config.BLOCK_CLASS_NAME;
+	}
+}
