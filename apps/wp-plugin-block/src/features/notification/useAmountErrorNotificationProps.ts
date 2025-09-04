0a61ecea7f558fc714a88561d65172448c81e68e
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-09-04 04:55:15 +0000

    refactor(wp-plugin): 画面で入力されている価格の値が不正なフォーマットの時に通知するコンポーネントとその引数を返すhookを追加

---
+++ apps/wp-plugin-block/src/features/notification/useAmountErrorNotificationProps.ts
@@ -0,0 +1,19 @@
+import { useMemo } from '@wordpress/element';
+import { ScreenNotifier } from '../../infrastructure/notification/ScreenNotifier';
+import { TextProvider } from '../../lib/i18n/TextProvider';
+import { useSellingPriceAmount } from '../../provider/selling-price-amount/useSellingPriceAmount';
+import { AmountErrorNotificationProps } from './AmountErrorNotification';
+
+export const useAmountErrorNotificationProps = (): AmountErrorNotificationProps => {
+	const { sellingPriceAmount } = useSellingPriceAmount();
+
+	const isError = sellingPriceAmount === null; // 販売価格の値がnullの場合、不正な文字列が入力されたものとみなす
+	const screenNotifier = useMemo( () => new ScreenNotifier(), [] );
+	const textProvider = useMemo( () => new TextProvider(), [] );
+
+	return {
+		isError,
+		screenNotifier,
+		textProvider,
+	};
+};
