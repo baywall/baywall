9cc1a29fbe8c75aecada7ed2808c2b160ea2ad7c
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-09-07 09:44:08 +0000

    refactor(wp-plugin): ResolvePostをvalue objectを使ってリファクタ

--- apps/wp-plugin-php/src/Application/UseCase/ResolvePost.php
+++ apps/wp-plugin-php/src/Application/UseCase/ResolvePost.php
@@ -3,55 +3,106 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Application\UseCase;
 
-use Cornix\Serendipity\Core\Application\Dto\TokenDto;
+use Cornix\Serendipity\Core\Application\Logging\AppLogger;
 use Cornix\Serendipity\Core\Application\Service\UserAccessChecker;
-use Cornix\Serendipity\Core\Application\UseCase\GetPostDto;
-use Cornix\Serendipity\Core\Application\UseCase\GetPayableTokens;
+use Cornix\Serendipity\Core\Domain\Entity\Post;
+use Cornix\Serendipity\Core\Domain\Entity\Token;
+use Cornix\Serendipity\Core\Domain\Repository\ChainRepository;
+use Cornix\Serendipity\Core\Domain\Repository\PostRepository;
+use Cornix\Serendipity\Core\Domain\Repository\TokenRepository;
+use Cornix\Serendipity\Core\Domain\Service\PostTitleProvider;
+use Cornix\Serendipity\Core\Domain\Specification\ChainsFilter;
+use Cornix\Serendipity\Core\Domain\Specification\TokensFilter;
+use Cornix\Serendipity\Core\Domain\ValueObject\PostId;
 
 class ResolvePost {
 
-	private GetPostDto $get_post_dto;
-	private GetPayableTokens $get_payable_tokens;
+	private AppLogger $logger;
 	private UserAccessChecker $user_access_checker;
+	private PostRepository $post_repository;
+	private ChainRepository $chain_repository;
+	private TokenRepository $token_repository;
+	private PostTitleProvider $post_title_provider;
 
 	public function __construct(
-		GetPostDto $get_post_dto,
-		GetPayableTokens $get_payable_tokens,
-		UserAccessChecker $user_access_checker
+		AppLogger $logger,
+		UserAccessChecker $user_access_checker,
+		PostRepository $post_repository,
+		ChainRepository $chain_repository,
+		TokenRepository $token_repository,
+		PostTitleProvider $post_title_provider
 	) {
-		$this->get_post_dto        = $get_post_dto;
-		$this->get_payable_tokens  = $get_payable_tokens;
+		$this->logger              = $logger;
 		$this->user_access_checker = $user_access_checker;
+		$this->post_repository     = $post_repository;
+		$this->chain_repository    = $chain_repository;
+		$this->token_repository    = $token_repository;
+		$this->post_title_provider = $post_title_provider;
 	}
 
-
 	public function handle( array $root_value, array $args ) {
-		$post_dto = $this->get_post_dto->handle( $args['postId'] );
-
+		$post_id = PostId::from( $args['postId'] );
 		// 投稿を閲覧できる権限があることをチェック
-		$this->user_access_checker->checkCanViewPost( $post_dto->id );
+		$this->user_access_checker->checkCanViewPost( $post_id->value() );
 
-		$payable_tokens_callback = function () use ( $root_value, $post_dto ) {
-			$payable_token_dtos = $this->get_payable_tokens->handle( $post_dto->id );
+		// 投稿の情報を取得
+		$post = $this->post_repository->get( $post_id );
 
+		$payable_tokens_callback = function () use ( $root_value, $post ) {
 			return array_map(
-				fn( TokenDto $token_dto ) => $root_value['token'](
+				fn( Token $token ) => $root_value['token'](
 					$root_value,
 					array(
-						'chainId' => $token_dto->chain_id,
-						'address' => $token_dto->address,
+						'chainId' => $token->chainId()->value(),
+						'address' => $token->address()->value(),
 					)
 				),
-				$payable_token_dtos
+				$this->getPayableTokens( $post )
 			);
 		};
 
+		$post_title_callback = function () use ( $post ) {
+			return $this->post_title_provider->getPostTitle( $post->id() );
+		};
+
 		return array(
-			'id'             => $post_dto->id,
-			'title'          => $post_dto->title,
-			'sellingPrice'   => fn() => $root_value['sellingPrice']( $root_value, array( 'postId' => $post_dto->id ) ),
-			'sellingContent' => fn() => $root_value['sellingContent']( $root_value, array( 'postId' => $post_dto->id ) ),
+			'id'             => $post->id()->value(),
+			'title'          => $post_title_callback,
+			'sellingPrice'   => fn() => $root_value['sellingPrice']( $root_value, array( 'postId' => $post->id()->value() ) ),
+			'sellingContent' => fn() => $root_value['sellingContent']( $root_value, array( 'postId' => $post->id()->value() ) ),
 			'payableTokens'  => $payable_tokens_callback,
 		);
+	}
+
+	/** 指定した投稿で支払い可能なトークン一覧を取得します */
+	private function getPayableTokens( Post $post ) {
+		$selling_network_category_id = $post->sellingNetworkCategoryId();
+
+		if ( $selling_network_category_id === null ) {
+			$this->logger->warn( '[22EB42D3] Selling network category is null for post ID: ' . $post->id()->value() );
+			return array(); // 販売ネットワークカテゴリが設定されていない場合は空の配列を返す
+		}
+
+		// 投稿に設定されている販売ネットワークカテゴリかつ接続可能なチェーン一覧を取得
+		$payable_chains = ( new ChainsFilter() )
+			->byNetworkCategoryId( $selling_network_category_id )
+			->byConnectable()
+			->apply( $this->chain_repository->all() );
+
+		/** @var Token[] */
+		$result = array();
+
+		// 各チェーンに対して支払い可能なトークンを取得
+		$all_tokens = $this->token_repository->all();
+		foreach ( $payable_chains as $chain ) {
+			$payable_tokens = ( new TokensFilter() )
+				->byChainId( $chain->id() )
+				->byIsPayable( true )
+				->apply( $all_tokens );
+
+			array_push( $result, ...$payable_tokens );
+		}
+
+		return $result;
 	}
 }
