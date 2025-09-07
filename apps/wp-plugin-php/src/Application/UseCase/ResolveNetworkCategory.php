a474f09c868444455434ad79d8b8bffaee0356e1
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-09-07 06:10:49 +0000

    refactor(wp-plugin): NetworkCategoryResolverの処理をResolveNetworkCategoryに記述

---
+++ apps/wp-plugin-php/src/Application/UseCase/ResolveNetworkCategory.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Application\UseCase;
+
+use Cornix\Serendipity\Core\Application\Service\SymbolService;
+use Cornix\Serendipity\Core\Application\Service\UserAccessChecker;
+use Cornix\Serendipity\Core\Domain\Repository\ChainRepository;
+use Cornix\Serendipity\Core\Domain\Specification\ChainsFilter;
+use Cornix\Serendipity\Core\Domain\ValueObject\NetworkCategoryId;
+use Cornix\Serendipity\Core\Domain\ValueObject\Symbol;
+
+class ResolveNetworkCategory {
+
+	private ChainRepository $chain_repository;
+	private UserAccessChecker $user_access_checker;
+	private SymbolService $symbol_service;
+
+	public function __construct(
+		ChainRepository $chain_repository,
+		UserAccessChecker $user_access_checker,
+		SymbolService $symbol_service
+	) {
+		$this->chain_repository    = $chain_repository;
+		$this->user_access_checker = $user_access_checker;
+		$this->symbol_service      = $symbol_service;
+	}
+
+	public function handle( array $root_value, array $args ): array {
+
+		$network_category_id = NetworkCategoryId::from( $args['networkCategoryId'] );
+
+		$sellable_symbols_callback = function () {
+			$this->user_access_checker->checkCanCreatePost();   // 投稿を新規作成できる権限が必要
+
+			return array_map(
+				fn( Symbol $symbol ) => $symbol->value(),
+				$this->symbol_service->getSellableSymbols()
+			);
+		};
+
+		// ネットワークカテゴリで絞り込んだチェーン一覧を取得
+		$chains_filter = ( new ChainsFilter() )->byNetworkCategoryId( $network_category_id );
+		$chains        = $chains_filter->apply( $this->chain_repository->all() );
+
+		$chains_callback = function () use ( $root_value, $chains ) {
+			return array_map(
+				function ( $chain ) use ( $root_value ) {
+					return $root_value['chain']( $root_value, array( 'chainId' => $chain->id()->value() ) );
+				},
+				$chains
+			);
+		};
+
+		return array(
+			'id'              => $network_category_id->value(),
+			'chains'          => $chains_callback,
+			'sellableSymbols' => $sellable_symbols_callback,
+		);
+	}
+}
