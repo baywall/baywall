2baa9eb50d52b2ff88987e188fd343703660e06e
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-09-09 07:52:14 +0000

    refactor(wp-plugin): SaveSellerのロジックをResolveSetSellerAgreedTermsへ移動

--- apps/wp-plugin-php/src/Application/UseCase/ResolveSetSellerAgreedTerms.php
+++ apps/wp-plugin-php/src/Application/UseCase/ResolveSetSellerAgreedTerms.php
@@ -4,31 +4,55 @@ declare(strict_types=1);
 namespace Cornix\Serendipity\Core\Application\UseCase;
 
 use Cornix\Serendipity\Core\Application\Service\UserAccessChecker;
-use Cornix\Serendipity\Core\Application\UseCase\SaveSeller;
+use Cornix\Serendipity\Core\Domain\Entity\Seller;
+use Cornix\Serendipity\Core\Domain\Repository\SellerRepository;
+use Cornix\Serendipity\Core\Domain\ValueObject\Signature;
+use Cornix\Serendipity\Core\Domain\ValueObject\TermsVersion;
+use Cornix\Serendipity\Core\Infrastructure\Terms\SellerTermsProvider;
+use Cornix\Serendipity\Core\Infrastructure\Web3\Ethers;
 
 class ResolveSetSellerAgreedTerms {
 
-	private SaveSeller $save_seller;
 	private UserAccessChecker $user_access_checker;
+	private SellerRepository $seller_repository;
+	private SellerTermsProvider $seller_terms_provider;
 
 	public function __construct(
-		SaveSeller $save_seller,
-		UserAccessChecker $user_access_checker
+		UserAccessChecker $user_access_checker,
+		SellerRepository $seller_repository,
+		SellerTermsProvider $seller_terms_provider
 	) {
-		$this->save_seller         = $save_seller;
-		$this->user_access_checker = $user_access_checker;
+		$this->user_access_checker   = $user_access_checker;
+		$this->seller_repository     = $seller_repository;
+		$this->seller_terms_provider = $seller_terms_provider;
 	}
 
 	public function handle( array $root_value, array $args ) {
 		$this->user_access_checker->checkHasAdminRole(); // 管理者権限が必要
 
-		/** @var int */
-		$version_value = $args['version'];
-		/** @var string */
-		$signature_value = $args['signature'];
+		$terms_version = TermsVersion::from( $args['version'] );
+		$signature     = Signature::from( $args['signature'] );
+
+		// 現在の販売者向け利用規約のバージョンと一致しない場合は例外を投げる
+		$current_version = $this->seller_terms_provider->currentVersion();
+		if ( $terms_version->equals( $current_version ) === false ) {
+			throw new \InvalidArgumentException( "[F2CC693C] Invalid terms version. Expected: {$current_version}, got: {$terms_version}" );
+		}
+
+		// 署名用メッセージを取得
+		$signing_message = $this->seller_terms_provider->getSigningMessage( $terms_version );
+		$seller_address  = Ethers::verifyMessage( $signing_message, $signature );
+		// TODO: 引数に string $expected_address を追加し、署名を検証するロジックを追加
 
 		// 販売者情報を保存
-		$this->save_seller->handle( $version_value, $signature_value );
+		$this->seller_repository->save(
+			new Seller(
+				$seller_address,
+				$terms_version,
+				$signing_message,
+				$signature,
+			)
+		);
 
 		return true;
 	}
