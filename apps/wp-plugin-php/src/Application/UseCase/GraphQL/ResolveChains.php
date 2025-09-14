972e2883ddc3d46269dfdac4ca2b65d76b2e8235
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-09-14 12:00:41 +0000

    chore(wp-plugin): GraphQLのResolverをUseCase/GraphQLディレクトリ以下に移動

--- apps/wp-plugin-php/src/Application/UseCase/GraphQL/ResolveChains.php
+++ apps/wp-plugin-php/src/Application/UseCase/GraphQL/ResolveChains.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Cornix\Serendipity\Core\Application\UseCase;
+namespace Cornix\Serendipity\Core\Application\UseCase\GraphQL;
 
 use Cornix\Serendipity\Core\Application\Service\UserAccessChecker;
 use Cornix\Serendipity\Core\Domain\Entity\Chain;
--- apps/wp-plugin-php/src/Application/UseCase/GraphQL/ResolveSaveChain.php
+++ apps/wp-plugin-php/src/Application/UseCase/GraphQL/ResolveSaveChain.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Cornix\Serendipity\Core\Application\UseCase;
+namespace Cornix\Serendipity\Core\Application\UseCase\GraphQL;
 
 use Cornix\Serendipity\Core\Application\Service\ChainIdChecker;
 use Cornix\Serendipity\Core\Application\Service\TransactionService;
