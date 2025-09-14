65d992d2aaad1725e2b99b2b1d600241bf61abca
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-09-14 06:37:58 +0000

    refactor(wp-plugin): OptionNameProviderをWordPress/Serviceディレクトリへ移動

--- apps/wp-plugin-php/src/Infrastructure/WordPress/Database/OptionGateway/PluginVersionOption.php
+++ apps/wp-plugin-php/src/Infrastructure/WordPress/Database/OptionGateway/PluginVersionOption.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Cornix\Serendipity\Core\Infrastructure\WordPress\Database\OptionGateway;
 
 use Cornix\Serendipity\Core\Lib\Option\StringOption;
-use Cornix\Serendipity\Core\Repository\Name\OptionNameProvider;
+use Cornix\Serendipity\Core\Infrastructure\WordPress\Service\OptionNameProvider;
 
 /**
  * プラグインバージョンを取得または保存するクラス
