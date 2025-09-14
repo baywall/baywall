b65d6c8c0022d6101239cd347686baef91334b72
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-09-14 09:45:11 +0000

    refactor(wp-plugin): 投稿内容を表すvalue objectを追加

---
+++ apps/wp-plugin-php/src/Domain/ValueObject/Content.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Domain\ValueObject;
+
+/** 記事の内容を表現するクラス */
+class Content implements \Stringable {
+	private function __construct( string $content_text ) {
+		$this->content_text = $content_text;
+	}
+	private string $content_text;
+
+	public static function from( string $content_text ): self {
+		return new self( $content_text );
+	}
+
+	public function value(): string {
+		return $this->content_text;
+	}
+
+	public function __toString(): string {
+		return $this->content_text;
+	}
+}
