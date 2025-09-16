916b50189ce86a4b563a4335abaaed782ea6f371
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-09-16 05:09:50 +0000

    refactor(wp-plugin): TransactionService::transactionalを定義

--- apps/wp-plugin-php/src/Application/Service/TransactionService.php
+++ apps/wp-plugin-php/src/Application/Service/TransactionService.php
@@ -8,4 +8,23 @@ abstract class TransactionService {
 	abstract public function beginTransaction(): void;
 	abstract public function commit(): void;
 	abstract public function rollback(): void;
+
+	/**
+	 * トランザクション内で処理を実行します。
+	 *
+	 * @template T
+	 * @param callable():T $func
+	 * @return T
+	 */
+	public function transactional( callable $func ) {
+		$this->beginTransaction();
+		try {
+			$result = $func();
+			$this->commit();
+			return $result;
+		} catch ( \Throwable $e ) {
+			$this->rollback();
+			throw $e;
+		}
+	}
 }
