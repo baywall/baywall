30da48e47ef485ae02e7ae8a6aa493f066d70466
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-09-16 02:54:12 +0000

    refactor(wp-plugin): WordPressPropertyProviderにRest APIのルートURLを返すメソッドを実装

--- apps/wp-plugin-php/src/Lib/Rest/RestProperty.php
+++ apps/wp-plugin-php/src/Lib/Rest/RestProperty.php
@@ -24,12 +24,7 @@ class RestProperty {
 	 * @return string
 	 */
 	public function graphQlUrl(): string {
-		// パーマリンク構造が基本の場合は、`/wp-json/`を含むURLではアクセスできないので`?rest_route=`を含むURLでAPIアクセスを行う。
-		// 参考: https://labor.ewigleere.net/2021/11/06/wordpress-restapi-404notfound-permalink-basic/
-
-		$wp_property   = new WordPressPropertyProvider();
-		$api_root_path = $wp_property->isDefaultPermalink() ? '/index.php?rest_route=/' : '/wp-json/';
-
-		return untrailingslashit( $wp_property->siteAddress() ) . $api_root_path . $this->namespace() . $this->graphQlRoute();
+		$wp_property = new WordPressPropertyProvider();
+		return untrailingslashit( $wp_property->apiRootUrl() ) . '/' . $this->namespace() . '/' . $this->graphQlRoute();
 	}
 }
