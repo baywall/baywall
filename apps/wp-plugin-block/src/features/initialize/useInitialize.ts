4e287183aa6baee74ef10acc7a592a12b40e29a8
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-01 04:12:56 +0000

    refactor(wp-plugin): 通貨シンボルの自動選択処理をuseSellingPriceSymbolSelectPropsに記述

--- apps/wp-plugin-block/src/features/initialize/useInitialize.ts
+++ apps/wp-plugin-block/src/features/initialize/useInitialize.ts
@@ -1,10 +1,9 @@
 import { useEffect } from '@wordpress/element';
 import { useBlockEditProps } from '../../provider/block-edit-props/useBlockEditProps';
 import { useBlockInitDataQuery } from '../../query/useBlockInitDataQuery';
-import { Amount, NetworkCategoryId, Symbol } from '@serendipity/lib-value-object';
+import { Amount, NetworkCategoryId } from '@serendipity/lib-value-object';
 import { useSelectedNetworkCategoryIdState } from '../selling-network-category/hooks/useSelectedNetworkCategoryIdState';
 import { useInputSellingPriceAmountState } from '../selling-price-amount/hooks/useInputSellingPriceAmountState';
-import { useSelectedSellingPriceSymbolState } from '../selling-price-symbol/hooks/useSelectedSellingPriceSymbolState';
 
 /**
  * 初期化処理
@@ -18,7 +17,6 @@ import { useSelectedSellingPriceSymbolState } from '../selling-price-symbol/hook
 export const useInitialize = (): void => {
 	useInitSellingNetworkCategoryId(); // ネットワークカテゴリIDの初期化
 	useInitSellingPriceAmount(); // 販売価格（数量）の初期化
-	useInitSellingPriceSymbol(); // 販売価格（通貨シンボル）の初期化
 };
 
 /** 画面で選択されているネットワークカテゴリIDを初期化します。 */
@@ -66,40 +64,4 @@ const useInitSellingPriceAmount = () => {
 		// Attributesに値がある場合はその値を、存在しない場合は初期値として0を設定
 		setSellingPriceAmount( ( prev ) => ( prev !== undefined ? prev : Amount.from( sellingAmount ?? '0' ) ) );
 	}, [ sellingAmount, data, setSellingPriceAmount ] );
-};
-
-/** 画面で選択されている販売価格（通貨シンボル）を初期化します。 */
-const useInitSellingPriceSymbol = () => {
-	const {
-		attributes: { sellingSymbol },
-	} = useBlockEditProps();
-	const { data } = useBlockInitDataQuery();
-	const [ selectedNetworkCategoryId ] = useSelectedNetworkCategoryIdState();
-	const [ , setSellingPriceSymbol ] = useSelectedSellingPriceSymbolState();
-
-	useEffect( () => {
-		if ( data === undefined ) {
-			return; // データ取得前は何もしない
-		}
-
-		setSellingPriceSymbol( ( prev ) => {
-			if ( prev !== undefined ) {
-				return prev; // 初期化済みの場合は何もしない
-			} else if ( selectedNetworkCategoryId === undefined ) {
-				return prev; // ネットワークカテゴリIDが未初期化の場合は何もしない
-			} else if ( selectedNetworkCategoryId === null ) {
-				return null; // ネットワークカテゴリIDがnullの場合はnullを設定
-			}
-
-			if ( sellingSymbol !== null ) {
-				return Symbol.from( sellingSymbol ); // Attributesに値がある場合はそれを優先して設定
-			}
-
-			// 先頭のシンボルを設定
-			return (
-				data.sellableNetworkCategories.find( ( c ) => c.id.equals( selectedNetworkCategoryId ) )
-					?.sellableSymbols[ 0 ] ?? null
-			);
-		} );
-	}, [ sellingSymbol, data, selectedNetworkCategoryId, setSellingPriceSymbol ] );
 };
--- apps/wp-plugin-block/src/features/selling-price-symbol/components/useSellingSymbolSelectProps.ts
+++ apps/wp-plugin-block/src/features/selling-price-symbol/components/useSellingSymbolSelectProps.ts
@@ -1,17 +1,49 @@
-import { useCallback, useMemo } from '@wordpress/element';
+import { useCallback, useEffect, useMemo } from '@wordpress/element';
 import { Symbol } from '@serendipity/lib-value-object';
 import { type SellingPriceSymbolSelectProps } from './SellingPriceSymbolSelect';
 import { useBlockInitDataQuery } from '../../../query/useBlockInitDataQuery';
 import { useSelectedSellingPriceSymbolState } from '../hooks/useSelectedSellingPriceSymbolState';
 import { useSellableSymbols } from '../hooks/useSellableSymbols';
+import { useSavedSellingSymbol } from '../../widget-attributes/useSavedSellingSymbol';
+import { useSavedSellingNetworkCategoryId } from '../../widget-attributes/useSavedSellingNetworkCategoryId';
+import { useSelectedNetworkCategoryIdState } from '../../selling-network-category/hooks/useSelectedNetworkCategoryIdState';
 
 export const useSellingPriceSymbolSelectProps = (): SellingPriceSymbolSelectProps => {
+	useAutoSelectValue();
+
 	return {
 		value: useValue(),
 		onChange: useOnChange(),
 		disabled: useDisabled(),
 		options: useOptions(),
 	};
+};
+
+/** 販売価格の通貨シンボルを自動的に選択します */
+const useAutoSelectValue = (): void => {
+	const savedSellingSymbol = useSavedSellingSymbol();
+	const [ selectedSymbol, setSelectedSymbol ] = useSelectedSellingPriceSymbolState();
+	const options = useOptions();
+
+	useEffect( () => {
+		if ( selectedSymbol === undefined && !! savedSellingSymbol ) {
+			// 初期化処理: 保存した通貨シンボルが存在する場合はロード
+			setSelectedSymbol( savedSellingSymbol ); // 本来選択できない通貨シンボルが設定される可能性あり
+			return;
+		} else if ( options === undefined ) {
+			return; // データ取得前は何もしない
+		}
+
+		if ( options === null || options.length === 0 ) {
+			// 選択可能な選択肢が存在しないにも関わらずnullが設定されていない場合はnullを設定する
+			if ( selectedSymbol !== null ) {
+				setSelectedSymbol( null );
+			}
+		} else if ( selectedSymbol && ! options.find( ( o ) => o.value === selectedSymbol.value ) ) {
+			// 選択可能な選択肢以外を選択している場合は先頭の選択肢を設定する
+			setSelectedSymbol( Symbol.from( options[ 0 ].value ) );
+		}
+	}, [ savedSellingSymbol, options, selectedSymbol, setSelectedSymbol ] );
 };
 
 const useValue = (): NonNullable< SellingPriceSymbolSelectProps[ 'value' ] > => {
@@ -36,7 +68,10 @@ const useDisabled = (): boolean => {
 };
 
 const useOptions = (): SellingPriceSymbolSelectProps[ 'options' ] => {
+	const savedSellingNetworkCategoryId = useSavedSellingNetworkCategoryId();
+	const [ selectedNetworkCategoryId ] = useSelectedNetworkCategoryIdState();
 	const sellableSymbols = useSellableSymbols();
+	const [ selectedSellingPriceSymbol ] = useSelectedSellingPriceSymbolState();
 
 	return useMemo( () => {
 		if ( sellableSymbols === undefined ) {
@@ -45,9 +80,33 @@ const useOptions = (): SellingPriceSymbolSelectProps[ 'options' ] => {
 			return null;
 		}
 
-		return sellableSymbols.map( ( symbol ) => ( {
+		const result = sellableSymbols.map( ( symbol ) => ( {
 			label: symbol.value,
 			value: symbol.value,
+			disabled: false,
 		} ) );
-	}, [ sellableSymbols ] );
+
+		// 保存時のネットワークカテゴリIDと同じネットワークカテゴリが画面で選択されているかどうかを取得
+		const isLoadedNetworkCategorySelected =
+			savedSellingNetworkCategoryId &&
+			selectedNetworkCategoryId &&
+			savedSellingNetworkCategoryId.equals( selectedNetworkCategoryId );
+		// 選択されている通貨シンボルが一覧に存在しないかどうかを取得
+		const isSelectedSymbolExists =
+			selectedSellingPriceSymbol && sellableSymbols.find( ( s ) => s.equals( selectedSellingPriceSymbol ) );
+
+		// 保存時と同じネットワークカテゴリが選択されており、選択済みの通貨シンボルが一覧に存在しない場合は選択肢に追加
+		if ( isLoadedNetworkCategorySelected && ! isSelectedSymbolExists ) {
+			if ( ! selectedSellingPriceSymbol ) {
+				throw new Error( '[5A02B58D]' ); // ここは通らない
+			}
+			result.unshift( {
+				label: selectedSellingPriceSymbol.value,
+				value: selectedSellingPriceSymbol.value,
+				disabled: true,
+			} );
+		}
+
+		return result;
+	}, [ savedSellingNetworkCategoryId, selectedNetworkCategoryId, sellableSymbols, selectedSellingPriceSymbol ] );
 };
