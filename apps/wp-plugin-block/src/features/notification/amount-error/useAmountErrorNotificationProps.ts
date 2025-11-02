25bc7fbbeeb7c1183d0cf793d3b271001b255f25
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-02 08:52:55 +0000

    refactor(wp-plugin): 入力された金額が不正な値かどうかを取得するisInvalidInputAmountを追加

--- apps/wp-plugin-block/src/features/notification/amount-error/useAmountErrorNotificationProps.ts
+++ apps/wp-plugin-block/src/features/notification/amount-error/useAmountErrorNotificationProps.ts
@@ -2,15 +2,15 @@ import { useMemo } from '@wordpress/element';
 import { ScreenNotifier } from '../../../lib/gutenberg/notification/ScreenNotifier';
 import { AmountErrorNotificationProps } from './AmountErrorNotification';
 import { useInputSellingPriceAmountState } from '../../selling-price-amount/hooks/useInputSellingPriceAmountState';
+import { isInvalidInputAmount } from './lib/isInvalidInputAmount';
 
 export const useAmountErrorNotificationProps = (): AmountErrorNotificationProps => {
-	const [ sellingPriceAmount ] = useInputSellingPriceAmountState();
-
-	const isError = sellingPriceAmount === null; // 販売価格の値がnullの場合、不正な文字列が入力されたものとみなす
-	const screenNotifier = useMemo( () => new ScreenNotifier(), [] );
-
 	return {
-		isError,
-		screenNotifier,
+		isError: useIsError(),
+		screenNotifier: useMemo( () => new ScreenNotifier(), [] ),
 	};
+};
+
+const useIsError = (): boolean => {
+	return isInvalidInputAmount( useInputSellingPriceAmountState()[ 0 ] );
 };
