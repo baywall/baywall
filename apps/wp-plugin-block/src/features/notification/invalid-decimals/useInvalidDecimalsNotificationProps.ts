df790f89476d743c24f15f0b200f1ee1010c94f2
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-03 02:21:19 +0000

    refactor(wp-plugin): useSelectedSymbolsMaxDecimalsの処理をuseInvalidDecimalsNotificationPropsへ移動

--- apps/wp-plugin-block/src/features/notification/invalid-decimals/useInvalidDecimalsNotificationProps.ts
+++ apps/wp-plugin-block/src/features/notification/invalid-decimals/useInvalidDecimalsNotificationProps.ts
@@ -2,8 +2,11 @@ import { useMemo } from 'react';
 import { ScreenNotifier } from '../../../lib/gutenberg/notification/ScreenNotifier';
 import { InvalidDecimalsNotificationProps } from './InvalidDecimalsNotification';
 import { useInputSellingPriceAmountState } from '../../selling-price-amount/hooks/useInputSellingPriceAmountState';
-import { useSelectedSymbolsMaxDecimals } from './hooks/useSelectedSymbolsMaxDecimals';
 import { useIsDecimalPlacesError } from './hooks/useIsDecimalPlacesError';
+import { useBlockInitDataQuery } from '../../../query/useBlockInitDataQuery';
+import { useSelectedNetworkCategoryIdState } from '../../selling-network-category/hooks/useSelectedNetworkCategoryIdState';
+import { useSelectedSellingPriceSymbolState } from '../../selling-price-symbol/hooks/useSelectedSellingPriceSymbolState';
+import { getMaxDecimals } from './lib/getMaxDecimals';
 
 export const useInvalidDecimalsNotificationProps = (): InvalidDecimalsNotificationProps => {
 	return {
@@ -14,7 +17,31 @@ export const useInvalidDecimalsNotificationProps = (): InvalidDecimalsNotificati
 
 const useIsError = (): boolean => {
 	const [ inputAmount ] = useInputSellingPriceAmountState();
-	const maxDecimals = useSelectedSymbolsMaxDecimals();
+	const { data } = useBlockInitDataQuery();
+	const [ selectedNetworkCategoryId ] = useSelectedNetworkCategoryIdState();
+	const [ selectedSellingPriceSymbol ] = useSelectedSellingPriceSymbolState();
+
+	const maxDecimals = useMemo( () => {
+		if (
+			data === undefined ||
+			selectedNetworkCategoryId === undefined ||
+			selectedSellingPriceSymbol === undefined
+		) {
+			return undefined;
+		} else if ( selectedNetworkCategoryId === null || selectedSellingPriceSymbol === null ) {
+			return null;
+		}
+
+		// 同一ネットワークカテゴリのToken一覧から最大小数点以下桁数を取得できる場合はその値を返す
+		const networkTokens = data.tokens.filter( ( t ) => t.networkCategoryId.equals( selectedNetworkCategoryId ) );
+		const networkMaxDecimals = getMaxDecimals( selectedSellingPriceSymbol, networkTokens );
+		if ( networkMaxDecimals ) {
+			return networkMaxDecimals;
+		}
+
+		// 見つからない場合は全ネットワークから最大小数点以下桁数を取得して返す
+		return getMaxDecimals( selectedSellingPriceSymbol, data.tokens );
+	}, [ data, selectedNetworkCategoryId, selectedSellingPriceSymbol ] );
 
 	return useIsDecimalPlacesError( inputAmount, maxDecimals );
 };
