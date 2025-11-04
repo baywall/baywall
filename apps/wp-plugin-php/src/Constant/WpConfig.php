f3329b5d3c5b2c23c9826f4cbdff1f80aa78ef96
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-04 11:46:13 +0000

    refactor(wp-plugin): ペイウォールブロックのクラス名をWpConfigに定義

---
+++ apps/wp-plugin-php/src/Constant/WpConfig.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Constant;
+
+/**
+ * WordPressにのみ関連する設定値を取得するためのクラス
+ */
+class WpConfig {
+	/**
+	 * ペイウォールブロックに付与するHTMLのCSSクラス名
+	 *
+	 * ※ TypeScript側と整合性を取ること
+	 */
+	public const PAYWALL_BLOCK_CLASS_NAME = 'ae6cefc4-82d4-4220-840b-d74538ea7284';
+}
