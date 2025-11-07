f5084d24038f1a4aeafb63593a1e48e2258c166a
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-07 02:50:48 +0000

    refactor(wp-plugin): PHP8.0以降でも名前空間を取得できるようにNamespaceParserを調整

--- apps/wp-plugin-php/src/Infrastructure/Util/NamespaceParser.php
+++ apps/wp-plugin-php/src/Infrastructure/Util/NamespaceParser.php
@@ -19,8 +19,18 @@ class NamespaceParser {
 				$namespace_found = true;
 			}
 			if ( $namespace_found ) {
-				if ( is_array( $token ) && ( in_array( $token[0], array( T_STRING, T_NS_SEPARATOR ) ) ) ) {
-					$namespace .= $token[1];
+				if ( is_array( $token ) ) {
+					// PHP8.0以降の新しいトークンタイプに対応
+					if ( defined( 'T_NAME_QUALIFIED' ) && $token[0] === T_NAME_QUALIFIED ) {
+						$namespace .= $token[1];
+					} elseif ( defined( 'T_NAME_FULLY_QUALIFIED' ) && $token[0] === T_NAME_FULLY_QUALIFIED ) {
+						$namespace .= $token[1];
+					} elseif ( defined( 'T_NAME_RELATIVE' ) && $token[0] === T_NAME_RELATIVE ) {
+						$namespace .= $token[1];
+					} elseif ( in_array( $token[0], array( T_STRING, T_NS_SEPARATOR ), true ) ) {
+						// PHP7.4以前の互換性を保持
+						$namespace .= $token[1];
+					}
 				} elseif ( $token === ';' || $token === '{' ) {
 					$namespace_found = false;
 				}
