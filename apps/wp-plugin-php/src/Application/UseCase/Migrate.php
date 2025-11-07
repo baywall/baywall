a60ecd7c539774d5df98c6aab1209e50f765850e
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-07 01:08:44 +0000

    refactor(wp-plugin): Migrateクラスを書き直し

---
+++ apps/wp-plugin-php/src/Application/UseCase/Migrate.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Application\UseCase;
+
+use Cornix\Serendipity\Core\Application\Logging\AppLogger;
+use Cornix\Serendipity\Core\Infrastructure\WordPress\Database\Migration\MigrationLocator;
+use Cornix\Serendipity\Core\Infrastructure\WordPress\ValueObject\PluginVersion;
+use DI\Container;
+
+/** データベースのマイグレーションを実行するクラス */
+class Migrate {
+
+	private MigrationLocator $locator;
+	private AppLogger $logger;
+
+	public function __construct( Container $container ) {
+		$this->locator = new MigrationLocator( $container );
+		$this->logger  = $container->get( AppLogger::class );
+	}
+
+	public function handle( string $prev_version, string $target_version ): void {
+		$migrations = $this->locator->get( PluginVersion::from( $prev_version ), PluginVersion::from( $target_version ) );
+
+		$rollbacks = array();
+		foreach ( $migrations as $migration ) {
+			try {
+				$rollbacks[] = $migration;
+				// マイグレーションを実行
+				$migration->up();
+				// 一番最初のマイグレーション処理でログレベルが設定されるので、ログ出力はそれ以降に行う
+				$this->logger->info( '[E923FBC1] Applied migration: ' . get_class( $migration ) );
+			} catch ( \Throwable $e ) {
+				$this->logger->error( $e );
+				$this->logger->info( '[B9A8BB31] Rolling back applied migrations...' );
+				foreach ( array_reverse( $rollbacks ) as $rollback ) {
+					try {
+						$this->logger->info( '[60697478] Rolling back migration: ' . get_class( $rollback ) );
+						$rollback->down();
+					} catch ( \Throwable $ex ) {
+						// ロールバック中に発生したエラーは再スローせずにログ出力だけ行う
+						$this->logger->error( $ex );
+					}
+				}
+				throw $e;
+			}
+		}
+	}
+}
