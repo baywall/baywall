cd54b7d220b9c0013d6a5064db7ccd495a13410d
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-08 09:31:19 +0000

    refactor(wp-plugin): テーブルスキーマ変更に伴う修正

--- apps/wp-plugin-php/src/Infrastructure/WordPress/Database/ValueObject/AppContractTableRecord.php
+++ apps/wp-plugin-php/src/Infrastructure/WordPress/Database/ValueObject/AppContractTableRecord.php
@@ -7,27 +7,18 @@ use stdClass;
 
 class AppContractTableRecord extends TableRecordBase {
 	public function __construct( stdClass $record ) {
-		$record->chain_id             = (int) $record->chain_id;
-		$record->crawled_block_number = $record->crawled_block_number === null ? null : (int) $record->crawled_block_number;
+		$record->chain_id = (int) $record->chain_id;
 
 		$this->import( $record );
 	}
 
 	protected int $chain_id;
 	protected string $address;
-	protected ?int $crawled_block_number;
-	protected ?string $crawled_block_number_updated_at;
 
 	public function chainIdValue(): int {
 		return $this->chain_id;
 	}
 	public function addressValue(): string {
 		return $this->address;
-	}
-	public function crawledBlockNumberValue(): ?int {
-		return $this->crawled_block_number;
-	}
-	public function crawledBlockNumberUpdatedAtValue(): ?string {
-		return $this->crawled_block_number_updated_at;
 	}
 }
