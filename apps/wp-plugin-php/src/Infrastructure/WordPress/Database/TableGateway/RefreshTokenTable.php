99d4a6c41edc818291fcaf7b50a1e53c73efd6b2
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-11 12:37:56 +0000

    refactor(wp-plugin): RefreshTokenTableのwpdbをMyWpdbに置き換え

--- apps/wp-plugin-php/src/Infrastructure/WordPress/Database/TableGateway/RefreshTokenTable.php
+++ apps/wp-plugin-php/src/Infrastructure/WordPress/Database/TableGateway/RefreshTokenTable.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Infrastructure\WordPress\Database\TableGateway;
 
+use Cornix\Serendipity\Core\Infrastructure\WordPress\Database\MyWpdb;
 use Cornix\Serendipity\Core\Infrastructure\WordPress\Database\TableNameProvider;
 use Cornix\Serendipity\Core\Infrastructure\WordPress\Database\ValueObject\RefreshTokenTableRecord;
 use Cornix\Serendipity\Core\Infrastructure\WordPress\Entity\RefreshTokenInfo;
@@ -11,22 +12,28 @@ use Cornix\Serendipity\Core\Infrastructure\WordPress\ValueObject\RefreshTokenHas
 /**
  * 認証用のリフレッシュトークンの情報を記録するテーブル
  */
-class RefreshTokenTable extends TableBase {
+class RefreshTokenTable {
 
-	public function __construct( \wpdb $wpdb, TableNameProvider $table_name_provider ) {
-		parent::__construct( $wpdb, $table_name_provider->refreshToken() );
+	private MyWpdb $wpdb;
+	private string $table_name;
+
+	public function __construct( MyWpdb $wpdb, TableNameProvider $table_name_provider ) {
+		$this->wpdb       = $wpdb;
+		$this->table_name = $table_name_provider->refreshToken();
 	}
 
 	public function get( RefreshTokenHash $refresh_token_hash ): ?RefreshTokenTableRecord {
-		$sql = <<<SQL
-			SELECT `refresh_token_hash`, `wallet_address`, `expires_at`, `revoked_at`
-			FROM `{$this->tableName()}`
-			WHERE `refresh_token_hash` = %s
-			LIMIT 1
-		SQL;
-		$sql = $this->prepare( $sql, $refresh_token_hash->value() );
+		$sql = $this->wpdb->prepare(
+			<<<SQL
+				SELECT `refresh_token_hash`, `wallet_address`, `expires_at`, `revoked_at`
+				FROM `{$this->table_name}`
+				WHERE `refresh_token_hash` = :refresh_token_hash
+				LIMIT 1
+			SQL,
+			array( ':refresh_token_hash' => $refresh_token_hash->value() )
+		);
 
-		$record = $this->safeGetRow( $sql );
+		$record = $this->wpdb->getRow( $sql );
 
 		return $record === null ? null : new RefreshTokenTableRecord( $record );
 	}
@@ -37,20 +44,16 @@ class RefreshTokenTable extends TableBase {
 			throw new \InvalidArgumentException( '[991AECA4] When adding a new refresh token, revoked_at must be null.' );
 		}
 
-		$sql = <<<SQL
-			INSERT INTO `{$this->tableName()}`
-			( `refresh_token_hash`, `wallet_address`, `expires_at`, `revoked_at` )
-			VALUES ( %s, %s, %s, %s )
-		SQL;
-		$sql = $this->prepare(
-			$sql,
-			$refresh_token_info->refreshTokenHash()->value(),
-			$refresh_token_info->walletAddress()->value(),
-			$refresh_token_info->expiresAt()->toMySqlValue(),
-			null // 追加時はrevoked_atはNULLで登録
+		$result = $this->wpdb->insert(
+			$this->table_name,
+			array(
+				'refresh_token_hash' => $refresh_token_info->refreshTokenHash()->value(),
+				'wallet_address'     => $refresh_token_info->walletAddress()->value(),
+				'expires_at'         => $refresh_token_info->expiresAt()->toMySqlValue(),
+				'revoked_at'         => null, // 追加時はrevoked_atはNULLで登録
+			)
 		);
 
-		$result = $this->safeQuery( $sql );
 		if ( $result !== 1 ) {
 			throw new \RuntimeException( '[2005DFF8] Failed to insert refresh token record.' );
 		}
