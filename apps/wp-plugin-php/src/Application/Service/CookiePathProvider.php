4b82ec100216d82b4623d7fe88678bb57f7ab210
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-12 12:20:07 +0000

    refactor(wp-plugin): アクセストークンはCookieに書き込まないので定義をCookiePathProviderから削除

--- apps/wp-plugin-php/src/Application/Service/CookiePathProvider.php
+++ apps/wp-plugin-php/src/Application/Service/CookiePathProvider.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 namespace Cornix\Serendipity\Core\Application\Service;
 
 interface CookiePathProvider {
-	/** Cookieに書き込むアクセストークンのパスを取得します */
-	public function accessToken(): string;
-
 	/** Cookieに書き込むリフレッシュトークンのパスを取得します */
 	public function refreshToken(): string;
 }
