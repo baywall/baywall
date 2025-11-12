c153cf922ee7c2d31311a64d631eddcfa05b3eb6
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-12 06:07:52 +0000

    refactor(wp-plugin): JWT形式のアクセストークンを表すクラスを追加

---
+++ apps/wp-plugin-php/src/Domain/ValueObject/Base/AccessToken.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Domain\ValueObject\Base;
+
+/**
+ * アクセストークンを表す基底クラス
+ */
+abstract class AccessToken extends StringValueObject {}
---
+++ apps/wp-plugin-php/src/Domain/ValueObject/JwtAccessToken.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Domain\ValueObject;
+
+use Cornix\Serendipity\Core\Domain\ValueObject\Base\AccessToken;
+
+/**
+ * JWT形式のアクセストークンを表すクラス
+ */
+final class JwtAccessToken extends AccessToken {
+
+	private function __construct( string $jwt_access_token_value ) {
+		parent::__construct( $jwt_access_token_value );
+
+		$this->checkJwtFormat( $jwt_access_token_value );
+	}
+
+	public static function from( string $jwt_access_token_value ): self {
+		return new self( $jwt_access_token_value );
+	}
+
+	private function checkJwtFormat( string $jwt_access_token_value ): void {
+		// 簡易チェック。署名等はチェックしない
+		if ( count( explode( '.', $jwt_access_token_value ) ) !== 3 ) {
+			throw new \InvalidArgumentException( '[98936770] Invalid JWT access token format. ' . $jwt_access_token_value );
+		}
+	}
+}
