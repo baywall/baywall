6f2123055c609176fd560d257667e8e1c5fcaf08
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-11-12 09:05:39 +0000

    refactor(wp-plugin): アクセストークンはCookieに書き込まないのでCookieExpirationProviderから定義を削除

--- apps/wp-plugin-php/src/Application/Service/CookieExpirationProvider.php
+++ apps/wp-plugin-php/src/Application/Service/CookieExpirationProvider.php
@@ -8,11 +8,6 @@ use Cornix\Serendipity\Core\Constant\Config;
 // ブラウザに書き込むCookieの有効期限を取得するクラス
 class CookieExpirationProvider {
 
-	/** Cookieに書き込むアクセストークンの有効期限(秒)を取得します */
-	public function accessToken(): int {
-		return Config::ACCESS_TOKEN_EXPIRATION;
-	}
-
 	/** Cookieに書き込むリフレッシュトークンの有効期限(秒)を取得します */
 	public function refreshToken(): int {
 		return Config::REFRESH_TOKEN_EXPIRATION;
