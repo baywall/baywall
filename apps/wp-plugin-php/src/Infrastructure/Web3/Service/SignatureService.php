4772a0008f6d92d844bd08d29fbbf31d0be3b0df
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-12-25 01:08:06 +0000

    refactor(wp-plugin): WalletServiceをDomainから削除、InfrastructureにSignatureServiceとしてリネーム

--- apps/wp-plugin-php/src/Infrastructure/Web3/Service/SignatureService.php
+++ apps/wp-plugin-php/src/Infrastructure/Web3/Service/SignatureService.php
@@ -4,19 +4,19 @@ declare(strict_types=1);
 namespace Cornix\Serendipity\Core\Infrastructure\Web3\Service;
 
 use Cornix\Serendipity\Core\Domain\Entity\Base\Signer;
-use Cornix\Serendipity\Core\Domain\Service\WalletService;
 use Cornix\Serendipity\Core\Domain\ValueObject\Address;
 use Cornix\Serendipity\Core\Domain\ValueObject\Signature;
 use Cornix\Serendipity\Core\Domain\ValueObject\SigningMessage;
 use Cornix\Serendipity\Core\Infrastructure\Web3\Ethers;
 
-class WalletServiceImpl implements WalletService {
-	/** @inheritdoc */
+/** 署名関連のサービスクラス */
+class SignatureService {
+	/** 署名を行います */
 	public function signMessage( Signer $signer, SigningMessage $message ): Signature {
 		return Ethers::signMessage( $signer->privateKey(), $message );
 	}
 
-	/** @inheritdoc */
+	/** 署名からアドレスを復元します */
 	public function recoverAddress( SigningMessage $message, Signature $signature ): Address {
 		return Ethers::verifyMessage( $message, $signature );
 	}
